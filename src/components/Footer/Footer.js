58d88353eb60d335fdb5120e2c9c0b4c96ee90d4
Jhacolyte <toneth.tembrevilla@gmail.com>
2019-10-29 04:36:03 -0700

    added .gitignore

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer text-center">
+    <span>
+      Clicky Game © 2019 Copyright:&nbsp;
+      <a
+        href="https://github.com/jhacolyte"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Toneth Tembrevilla
+      </a>
+    </span>
+  </footer>
+);
+
+export default Footer;
